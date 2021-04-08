b4d8adbf79897f6379e78b03a58135b9b6087389
Ali-Tigui <75787851+Ali-Tigui@users.noreply.github.com>
2021-04-08 21:07:12 +0100

    add file

---
+++ scrpt.js
@@ -0,0 +1,18 @@
+setInterval(function(){
+    document.querySelector('div').style.top=(window.innerHeight-document.querySelector('div').offsetHeight)/2+ "px";
+    document.querySelector('div').style.left=(window.innerWidth-document.querySelector('div').offsetWidth)/2 +"px";
+},10)
+document.getElementById('btn').onclick=function(){
+    
+    var moy=(parseInt(document.querySelectorAll("input")[0].value)*2+parseInt(document.querySelectorAll("input")[1].value)*2+parseInt(document.querySelectorAll("input")[2].value)*3+parseInt(document.querySelectorAll("input")[3].value)*2+parseInt(document.querySelectorAll("input")[4].value)*1+parseInt(document.querySelectorAll("input")[5].value)*2+parseInt(document.querySelectorAll("input")[6].value)*0.5+parseInt(document.querySelectorAll("input")[7].value)*0.5+parseInt(document.querySelectorAll("input")[8].value)*1+parseInt(document.querySelectorAll("input")[9].value)*1+parseInt(document.querySelectorAll("input")[10].value)*1+parseInt(document.querySelectorAll("input")[11].value)*1)/17;
+        if (moy <10 ){
+            document.querySelector('.not').style.color="red";
+        }else if(moy<13){
+         document.querySelector('.not').style.color="#00249b";
+            
+        }else{
+            document.querySelector('.not').style.color="#043c0d";
+        }
+    document.querySelector('.not').textContent=moy;
+    
+}
