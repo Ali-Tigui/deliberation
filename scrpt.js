487b12693a6322f925e59d3e69198d0a4e4f1306
Ali-Tigui <75787851+Ali-Tigui@users.noreply.github.com>
2021-04-18 20:00:40 +0100

    Update scrpt.js

--- scrpt.js
+++ scrpt.js
@@ -4,7 +4,7 @@ setInterval(function(){
 },10)
 document.getElementById('btn').onclick=function(){
     
-    var moy=(parseInt(document.querySelectorAll("input")[0].value)*2+parseInt(document.querySelectorAll("input")[1].value)*2+parseInt(document.querySelectorAll("input")[2].value)*3+parseInt(document.querySelectorAll("input")[3].value)*2+parseInt(document.querySelectorAll("input")[4].value)*1+parseInt(document.querySelectorAll("input")[5].value)*2+parseInt(document.querySelectorAll("input")[6].value)*0.5+parseInt(document.querySelectorAll("input")[7].value)*0.5+parseInt(document.querySelectorAll("input")[8].value)*1+parseInt(document.querySelectorAll("input")[9].value)*1+parseInt(document.querySelectorAll("input")[10].value)*1+parseInt(document.querySelectorAll("input")[11].value)*1)/17;
+    var moy=(Number(document.querySelectorAll("input")[0].value)*2+Number(document.querySelectorAll("input")[1].value)*2+Number(document.querySelectorAll("input")[2].value)*3+Number(document.querySelectorAll("input")[3].value)*2+Number(document.querySelectorAll("input")[4].value)*1+Number(document.querySelectorAll("input")[5].value)*2+Number(document.querySelectorAll("input")[6].value)*0.5+Number(document.querySelectorAll("input")[7].value)*0.5+Number(document.querySelectorAll("input")[8].value)*1+Number(document.querySelectorAll("input")[9].value)*1+Number(document.querySelectorAll("input")[10].value)*1+Number(document.querySelectorAll("input")[11].value)*1)/17;
         if (moy <10 ){
             document.querySelector('.not').style.color="red";
         }else if(moy<13){
